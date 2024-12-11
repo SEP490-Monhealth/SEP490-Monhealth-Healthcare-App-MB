264c0f214797418b8d39eaa761775072fca5e89a
KhaiToir <khaipvse171817@fpt.edu.vn>
2024-12-11 23:59:31 +0700

    feat: coding schedule of month

--- src/app/(tabs)/schedule.tsx
+++ src/app/(tabs)/schedule.tsx
@@ -1,13 +1,19 @@
-import React from "react"
+import React, { useCallback, useState } from "react"
+
+import { View } from "react-native"
 
 import { Add } from "iconsax-react-native"
 
 import { Container } from "@/components/global/atoms"
 import { Header } from "@/components/global/organisms"
 
+import DateOfMonthList from "@/components/local/tabs/schedule/DateOfMonth"
+
 import { COLORS } from "@/constants/appConstants"
 
 function ScheduleScreen() {
+  const [currentDate, setCurrentDate] = useState(new Date())
+
   return (
     <Container>
       <Header
@@ -17,6 +23,10 @@ function ScheduleScreen() {
           url: "/foods"
         }}
       />
+
+      <View className="mt-6">
+        <DateOfMonthList date={currentDate} />
+      </View>
     </Container>
   )
 }
