fde401ebe8e9809db5a51db948490bc6ad715ef6
KhaiToir <khaipvse171817@fpt.edu.vn>
2024-12-19 15:47:58 +0700

    feat: coding bottom sheet for food detail

--- src/app/foods/[foodId]/details.tsx
+++ src/app/foods/[foodId]/details.tsx
@@ -1,21 +1,29 @@
-import React from "react"
+import React, { useRef } from "react"
 
-import { Text } from "react-native"
+import { Button, Text, View } from "react-native"
 
 import { useLocalSearchParams } from "expo-router"
 
 import LoadingScreen from "@/app/loading"
 
-import { Container, Content } from "@/components/global/atoms"
+import { Container, Content, HStack, VStack } from "@/components/global/atoms"
 import { Header, Section } from "@/components/global/organisms"
 
 import { NutritionFacts } from "@/components/local/foods"
 
 import { sampleFoodsData } from "@/constants/foods"
 
+import BottomSheet, { BottomSheetRefProps } from "../bottomSheet"
+
 function FoodDetailsScreen() {
   const { foodId } = useLocalSearchParams()
   const foodData = sampleFoodsData.find((item) => item.foodId === foodId)
+
+  const bottomSheetRef = useRef<BottomSheetRefProps>(null)
+
+  const openBottomSheet = () => {
+    bottomSheetRef.current?.scrollTo(-500)
+  }
 
   if (!foodData) {
     return <LoadingScreen />
@@ -26,17 +34,30 @@ function FoodDetailsScreen() {
       <Header back title="Chi tiết thức ăn" />
 
       <Content margin={false}>
-        <Text className="font-tbold text-xl text-typography">
-          {foodData.foodName}
-        </Text>
-        <Text className="font-tregular text-base text-secondary">
-          {foodData.portionSize} {foodData.portionWeight}{" "}
-          {foodData.measurementUnit}
-        </Text>
+        <HStack className="flex justify-between">
+          <View>
+            <Text className="font-tbold text-xl text-typography">
+              {foodData.foodName}
+            </Text>
 
+            <Text className="font-tregular text-base text-secondary">
+              {foodData.portionSize} {foodData.portionWeight}{" "}
+              {foodData.measurementUnit}
+            </Text>
+          </View>
+          <View>
+            <Button title="Mở" onPress={openBottomSheet} />
+          </View>
+        </HStack>
         <Section title="Thông tin dinh dưỡng" />
         <NutritionFacts nutritionData={foodData} />
       </Content>
+
+      <BottomSheet ref={bottomSheetRef}>
+        <VStack center>
+          <Text>Nội dung trong BottomSheet</Text>
+        </VStack>
+      </BottomSheet>
     </Container>
   )
 }
