21a5e7e9408e78dc82e4bde7ce540f7026c459bb
KhaiToir <khaipvse171817@fpt.edu.vn>
2025-01-15 17:43:01 +0700

    feat: coding page privacy setting

--- src/app/users/privacy-settings.tsx
+++ src/app/users/privacy-settings.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react"
 
-import { Text, View } from "react-native"
-
-import { useRouter } from "expo-router"
+import { Text } from "react-native"
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Eye, EyeSlash, Lock1 } from "iconsax-react-native"
@@ -19,37 +17,45 @@ import { Header } from "@/components/global/organisms"
 
 import { COLORS } from "@/constants/app"
 
-import { ResetPasswordType, resetPasswordSchema } from "@/schemas/userSchema"
+import { UpdatePasswordType, updatePasswordSchema } from "@/schemas/userSchema"
 
 function PrivacySettingScreen() {
   const [isLoading, setIsLoading] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
+  const [showNewPassword, setShowNewPassword] = useState(false)
   const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
   const {
     control,
     handleSubmit,
     formState: { errors }
-  } = useForm<ResetPasswordType>({
-    resolver: zodResolver(resetPasswordSchema),
+  } = useForm<UpdatePasswordType>({
+    resolver: zodResolver(updatePasswordSchema),
     defaultValues: {
       password: "",
+      newPassword: "",
       confirmPassword: ""
     }
   })
 
-  const onSubmit = (data: ResetPasswordType) => {
+  const onSubmit = (data: UpdatePasswordType) => {
     setIsLoading(true)
-    console.log("Submit:", data)
+    // console.log("Submit:", data)
+    const { password, newPassword } = data
+
+    const payload = { password, newPassword }
+
+    console.log("Submit Payload:", payload)
+
     setIsLoading(false)
   }
 
   return (
-    <Container>
+    <Container dismissKeyboard>
       <Header back label="Bảo mật " />
 
       <Content className="mt-2">
-        <VStack gap={12} className="mt-8">
+        <VStack gap={12}>
           <Controller
             name="password"
             control={control}
@@ -78,12 +84,39 @@ function PrivacySettingScreen() {
           />
 
           <Controller
-            name="confirmPassword"
+            name="newPassword"
             control={control}
             render={({ field: { onChange, value } }) => (
               <Input
                 value={value}
                 placeholder="Nhập mật khẩu mới"
+                onChangeText={onChange}
+                isSecure={!showNewPassword}
+                onToggleSecure={() => setShowNewPassword(!showNewPassword)}
+                startIcon={
+                  <Lock1 variant="Bold" size={20} color={COLORS.primary} />
+                }
+                endIcon={
+                  showNewPassword ? (
+                    <Eye variant="Bold" size={20} color={COLORS.primary} />
+                  ) : (
+                    <EyeSlash variant="Bold" size={20} color="#cbd5e1" />
+                  )
+                }
+                alwaysShowEndIcon
+                canClearText
+                errorMessage={errors.newPassword?.message}
+              />
+            )}
+          />
+
+          <Controller
+            name="confirmPassword"
+            control={control}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                value={value}
+                placeholder="Nhập lại mật khẩu"
                 onChangeText={onChange}
                 isSecure={!showConfirmPassword}
                 onToggleSecure={() =>
@@ -105,6 +138,17 @@ function PrivacySettingScreen() {
               />
             )}
           />
+          <VStack className="mt-4">
+            <Text className="font-tregular text-base text-accent">
+              Mật khẩu phải từ 6 đến 20 ký tự
+            </Text>
+            <Text className="font-tregular text-base text-accent">
+              Bao gồm số, chữ viết hoa, chữ viết thường
+            </Text>
+            <Text className="font-tregular text-base text-accent">
+              Bao gồm có ít nhất 1 ký tự đặc biệt
+            </Text>
+          </VStack>
         </VStack>
 
         <Button
