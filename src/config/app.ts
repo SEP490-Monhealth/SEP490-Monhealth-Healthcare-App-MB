85ab10094841f417b365300b504faeab8d012ef5
Van Huu Toan <vhtoan27@gmail.com>
2025-02-20 12:38:41 +0700

    feat: upgrade package versions

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,6 @@
 export const appConfig = {
   apiUrl:
-    "https://87be-2401-d800-9b8d-bfce-7503-1c0c-89b7-ba3a.ngrok-free.app/api/v1",
+    "https://733c-2405-4802-901a-b5d0-ec56-869d-bbfa-4bc2.ngrok-free.app/api/v1",
   theme: "light",
   language: "vi"
 }
