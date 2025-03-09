725505a55533e7521b2805ee9f9d7bcd7300f47c
Van Huu Toan <vhtoan27@gmail.com>
2025-03-09 14:37:13 +0700

    feat: adding sample data foods portion nutrition for testing

---
+++ src/constants/foods.tsx
@@ -0,0 +1,25 @@
+export const sameplFoodsData = [
+  {
+    foodId: "599cfc95-4108-4dcc-9c5a-077173251f8b",
+    userId: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+    foodType: [0],
+    mealType: [0, 1, 2],
+    dishType: [0],
+    name: "Cơm",
+    description:
+      "Cơm là món ăn chủ yếu trong bữa ăn Việt Nam, cung cấp tinh bột quan trọng. Cơm thường ăn kèm với các món mặn như thịt, cá và rau, tạo nên một bữa ăn đầy đủ dinh dưỡng, bổ sung năng lượng cho cơ thể",
+    allergies: [],
+    isPublic: true
+  },
+  {
+    foodId: "da89578e-9470-4b18-af4f-0bdcc011882f",
+    userId: "3026595f-1414-4b74-be8f-11b7f6e7f4f6",
+    foodType: [0, 1],
+    mealType: [0, 1, 2],
+    dishType: [0],
+    name: "Phở gà",
+    description:
+      "Phở gà là món ăn nhẹ nhàng, nước dùng thơm từ gà, ăn kèm với bánh phở và thịt gà. Món này có thể ăn vào buổi sáng hoặc bữa tối, rất thanh đạm nhưng đầy đủ dinh dưỡng",
+    allergies: []
+  }
+]
---
+++ src/constants/nutrition.tsx
@@ -0,0 +1,20 @@
+export const sampleNutritionData = [
+  {
+    nutritionId: "55e9c9ea-58eb-4c96-b307-bb7c194bd503",
+    calories: 130,
+    protein: 2.7,
+    carbs: 28.2,
+    fat: 0.3,
+    fiber: 0.4,
+    sugar: 0.1
+  },
+  {
+    nutritionId: "cb40c205-f322-4b00-b692-a3dd78ea7a0f",
+    calories: 420,
+    protein: 18,
+    carbs: 50,
+    fat: 10,
+    fiber: 2.5,
+    sugar: 2
+  }
+]
