1d832be339405f44a9cbf76c50b359554d1b8d0f
Van Huu Toan <vhtoan27@gmail.com>
2025-04-24 13:24:17 +0700

    feat: coding booking report detail

--- src/app/bookings/[bookingId]/report/index.tsx
+++ src/app/bookings/[bookingId]/report/index.tsx
@@ -1,12 +1,66 @@
 import React from "react"
 
-import { Text, View } from "react-native"
+import { Image, Text, View } from "react-native"
+
+import { useLocalSearchParams } from "expo-router"
+
+import { LoadingScreen } from "@/app/loading"
+
+import { Container, Content, Input, VStack } from "@/components/global/atoms"
+import { Header } from "@/components/global/organisms"
+
+import { useGetReportByBookingId } from "@/hooks/useReport"
 
 const ReportDetailsScreen = () => {
+  const { bookingId } = useLocalSearchParams<{ bookingId: string }>()
+
+  const { data: reportData } = useGetReportByBookingId(bookingId)
+
+  const currentReport = reportData?.[0]
+
+  console.log(JSON.stringify(currentReport, null, 2))
+
+  if (!reportData) {
+    return <LoadingScreen />
+  }
+
   return (
-    <View>
-      <Text>ReportDetailsScreen</Text>
-    </View>
+    <Container>
+      <Header back label="Báo cáo" />
+
+      <Content className="pt-2">
+        <VStack gap={12}>
+          <Input disabled value={currentReport?.reason} label="Lý do" />
+
+          {currentReport?.reason === "Khác (Thêm mô tả bên dưới)" && (
+            <Input
+              value={currentReport?.reason}
+              label="Thông tin bổ sung"
+              isMultiline
+              numberOfLines={6}
+            />
+          )}
+
+          <View>
+            <Text className="mb-1 ml-1 font-tregular text-base text-primary">
+              Hình ảnh
+            </Text>
+
+            <View className="flex-row flex-wrap gap-2">
+              {currentReport?.imageUrls.map((uri, index) => (
+                <View key={index} className="h-28 w-28">
+                  <Image
+                    source={{ uri }}
+                    resizeMode="cover"
+                    className="h-full w-full rounded-lg"
+                  />
+                </View>
+              ))}
+            </View>
+          </View>
+        </VStack>
+      </Content>
+    </Container>
   )
 }
 
