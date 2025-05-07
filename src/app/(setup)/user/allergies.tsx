0cba01f2e14e7b316804ad6b6002090a4d7c8e35
Van Huu Toan <vhtoan27@gmail.com>
2025-05-07 12:02:31 +0700

    feat: adding reference url setup allergies

--- src/app/(setup)/user/allergies.tsx
+++ src/app/(setup)/user/allergies.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { Linking, Text, TouchableOpacity } from "react-native"
+
 import { Control, FieldValues, useController } from "react-hook-form"
 
 import { Chip, ScrollArea, VStack } from "@/components/global/atoms"
@@ -18,6 +20,9 @@ function SetupAllergies({ control }: SetupAllergiesProps) {
     control
   })
 
+  const referenceUrl =
+    "https://www.vinmec.com/vie/bai-viet/8-loai-di-ung-thuc-pham-pho-bien-nhat-vi"
+
   const handleSelectAllergies = (allergy: string) => {
     const currentValue = field.value || []
 
@@ -28,21 +33,33 @@ function SetupAllergies({ control }: SetupAllergiesProps) {
     }
   }
 
+  const handleLinkPress = () => {
+    Linking.openURL(referenceUrl)
+  }
+
   return (
     <ScrollArea>
-      <VStack gap={12} className="pb-28">
-        {allergiesData.map((allergy) => (
-          <Chip
-            key={allergy.allergyId}
-            size="lg"
-            border
-            borderWidth={2}
-            label={allergy.name}
-            description={allergy.description}
-            selected={field.value?.includes(allergy.name)}
-            onPress={() => handleSelectAllergies(allergy.name)}
-          />
-        ))}
+      <VStack gap={20} className="pb-28">
+        <VStack gap={12}>
+          {allergiesData.map((allergy) => (
+            <Chip
+              key={allergy.allergyId}
+              size="lg"
+              border
+              borderWidth={2}
+              label={allergy.name}
+              description={allergy.description}
+              selected={field.value?.includes(allergy.name)}
+              onPress={() => handleSelectAllergies(allergy.name)}
+            />
+          ))}
+        </VStack>
+
+        <TouchableOpacity activeOpacity={1} onPress={handleLinkPress}>
+          <Text className="text-center font-tmedium text-secondary underline">
+            {referenceUrl}
+          </Text>
+        </TouchableOpacity>
       </VStack>
     </ScrollArea>
   )
